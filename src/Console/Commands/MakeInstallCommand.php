3c76ff417cac8e6e1ce173a1b91f316a74bde0c3
Carlos Puche <cpuche@wepa.es>
2022-10-07 16:51:33 +0100

    fix: instalation file

--- src/Console/Commands/MakeInstallCommand.php
+++ src/Console/Commands/MakeInstallCommand.php
@@ -40,14 +40,7 @@ class MakeInstallCommand extends Command
 	public function handle()
 	{
 		$filePath = __DIR__ . '/../../../install.zip';
-
-		if(file_exists($filePath)){
-			unlink($filePath);
-		}
-		
-		$zip = new ZipArchive();
-		
-		$filesToBackup = [
+		$files = [
 			resource_path('js/app.js'),
 			resource_path('css/app.css'),
 			base_path('tailwind.config.js'),
@@ -62,11 +55,32 @@ class MakeInstallCommand extends Command
 			base_path('config/permission.php'),
 		];
 		
+		foreach($files as $file) {
+			$relativeNameInFile = str_replace(base_path() . DIRECTORY_SEPARATOR,
+				'',
+				$file);
+			$this->warn($relativeNameInFile);
+		}
+		$this->warn('');
+		$this->warn('--------------------');
+		$this->confirm('Are you sure you want to replace the installation files? Check one by one from the list as they will be overwritten.');
+		if(file_exists($filePath)) {
+			unlink($filePath);
+		}
+		
+		$zip = new ZipArchive();
+		
 		if($zip->open($filePath, ZipArchive::CREATE) === true) {
-			foreach($filesToBackup as $file) {
-				$relativeNameInFile = str_replace(base_path() . DIRECTORY_SEPARATOR, '', $file);
-				$this->info('-- ' . $relativeNameInFile);
-				$zip->addFile($file, $relativeNameInFile);
+			foreach($files as $file) {
+				$relativeNameInFile = str_replace(base_path() . DIRECTORY_SEPARATOR,
+					'',
+					$file);
+				if(file_exists($file)) {
+					$this->info('-- ' . $relativeNameInFile);
+					$zip->addFile($file, $relativeNameInFile);
+				} else {
+					$this->warn('-- ' . $relativeNameInFile);
+				}
 			}
 		}
 		
