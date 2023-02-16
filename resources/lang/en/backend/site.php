55e63dfc489412e1db91ab971e2294d67541270a
Carlos Puche <cpuche@wepa.es>
2023-02-16 20:33:29 +0000

    fix: site translations

--- resources/lang/en/backend/site.php
+++ resources/lang/en/backend/site.php
@@ -2,5 +2,6 @@
 
 return [
     'edit_title' => 'Edit the site configuration',
+	'social_networks' => 'Social networks',
     'edit_summary' => 'Edit the information that will be displayed on the web page, do not forget that any changes will be reflected when you save the changes.',
 ];
--- resources/lang/es/backend/site.php
+++ resources/lang/es/backend/site.php
@@ -2,5 +2,6 @@
 
 return [
     'edit_title' => 'Editar la configuración del sitio',
+    'social_networks' => 'Redes sociales',
     'edit_summary' => 'Edite la información que se mostrará en la página web, no olvide que cualquier cambio se reflejará cuando guarde los cambios.',
 ];
