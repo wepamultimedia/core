26e7310447876d50ab00734cbcf14f42872f215b
Carlos Puche <cpuche@wepa.es>
2023-08-03 16:01:56 +0100

    Update Table.vue

--- resources/js/Components/Table.vue
+++ resources/js/Components/Table.vue
@@ -24,15 +24,15 @@ const props = defineProps({
 
 const searchInput = ref(usePage().props.ziggy?.query?.search || "");
 const {
-    data,
-    columns,
-    searchRoute,
-    viewRoute,
-    editRoute,
-    deleteRoute,
-    deleteTitle,
-    deleteMessage
-} = toRefs(props);
+          data,
+          columns,
+          searchRoute,
+          viewRoute,
+          editRoute,
+          deleteRoute,
+          deleteTitle,
+          deleteMessage
+      } = toRefs(props);
 
 const items = ref();
 
@@ -87,111 +87,111 @@ onMounted(() => {
                   w-full
                   text-left">
         <thead class="uppercase tracking-wider text-xs font-semibold">
-        <tr>
-            <th v-for="col in columns"
-                v-show="col.show ? showSizes[col.show] : true"
-                :class="[col.class, col.thClass]"
-                class="uppercase bg-skin-background px-5 py-3">
-                {{ col.label ? col.label : col.name ? __(col.name) : __(col) }}
-            </th>
-            <th v-if="showActions()"
-                class="bg-skin-background px-5 py-3 text-center min-w-max">
-                {{ __("actions") }}
-            </th>
-        </tr>
+            <tr>
+                <th v-for="col in columns"
+                    v-show="col.show ? showSizes[col.show] : true"
+                    :class="[col.class, col.thClass]"
+                    class="uppercase bg-skin-background px-5 py-3">
+                    {{ col.label ? col.label : col.name ? __(col.name) : __(col) }}
+                </th>
+                <th v-if="showActions()"
+                    class="bg-skin-background px-5 py-3 text-center min-w-max">
+                    {{ __("actions") }}
+                </th>
+            </tr>
         </thead>
         <tbody class="text-sm">
-        <tr v-if="searchRoute">
-            <td :colspan="countColumns()">
-                <Input v-model="searchInput"
-                       :placeholder="__('search')"
-                       class="input w-full text-sm sm:w-1/2 !rounded-none !border-none focus:!border-0 focus:!ring-0"
-                       name="search"
-                       type="text"
-                       @keyup.enter="search(searchInput)">
-                    <template #left>
-                        <div class="px-2">
-                            <inline-svg class="w-5 h-5 fill-skin-base"
-                                        src="/vendor/core/icons/solid/search.svg"></inline-svg>
-                        </div>
-                    </template>
-                </Input>
-            </td>
-        </tr>
-        <template v-for="item in data.data ? data.data : data"
-                  :key="item.id + '-row'">
-            <slot name="row">
-                <tr :class="{'even:even ': even, 'div-y' : divideX || divide}">
-                    <slot :item="item"
-                          name="row-content">
-                        <template v-for="col in columns"
-                                  :key="item.id + '-col'">
-                            <slot :item="item"
-                                  :name="col.name ? 'col-' + col.name : 'col-' + col">
-                                <td v-show="col.show ? showSizes[col.show] : true"
-                                    :class="[col.class, col.tdClass, {'div-x' : divideY || divide}]"
-                                    class="px-4 py-2">
-                                    <slot :item="item"
-                                          :name="col.name ? 'col-content-' + col.name : 'col-content-' + col">
-                                        {{
-                                            (item[col.name ? col.name : col] && (typeof item[col.name ? col.name
-                                                : col] !== "array" && typeof item[col.name
-                                                ? col.name
-                                                : col] !== "object"))
-                                                ? item[col.name ? col.name : col] : "..."
-                                        }}
-                                    </slot>
-                                </td>
-                            </slot>
+            <tr v-if="searchRoute">
+                <td :colspan="countColumns()">
+                    <Input v-model="searchInput"
+                           :placeholder="__('search')"
+                           class="input w-full text-sm sm:w-1/2 !rounded-none !border-none focus:!border-0 focus:!ring-0"
+                           name="search"
+                           type="text"
+                           @keyup.enter="search(searchInput)">
+                        <template #left>
+                            <div class="px-2">
+                                <inline-svg class="w-5 h-5 fill-skin-base"
+                                            src="/vendor/core/icons/solid/search.svg"></inline-svg>
+                            </div>
                         </template>
-                        <td v-if="showActions()"
-                            :class="{'div-y' : divideY || divide}"
-                            class="px-2 py-3 text-center">
-                            <slot :item="item"
-                                  name="action">
+                    </Input>
+                </td>
+            </tr>
+            <template v-for="item in data.data ? data.data : data"
+                      :key="item.id + '-row'">
+                <slot name="row">
+                    <tr :class="{'even:even ': even, 'div-y' : divideX || divide}">
+                        <slot :item="item"
+                              name="row-content">
+                            <template v-for="col in columns"
+                                      :key="item.id + '-col'">
                                 <slot :item="item"
-                                      name="action-view">
-                                    <Link v-if="viewRoute"
-                                          :href="route(viewRoute, {id:item.id})"
-                                          as="button"
-                                          class="p-1 w-8 h-6">
-                                        <icon class="fill-skin-base  w-5 h-5"
-                                              icon="eye"></icon>
-                                    </Link>
+                                      :name="col.name ? 'col-' + col.name : 'col-' + col">
+                                    <td v-show="col.show ? showSizes[col.show] : true"
+                                        :class="[col.class, col.tdClass, {'div-x' : divideY || divide}]"
+                                        class="px-4 py-2">
+                                        <slot :item="item"
+                                              :name="col.name ? 'col-content-' + col.name : 'col-content-' + col">
+                                            {{
+                                                (item[col.name ? col.name : col] && (typeof item[col.name ? col.name
+                                                                                                          : col] !== "array" && typeof item[col.name
+                                                                                                                                            ? col.name
+                                                                                                                                            : col] !== "object"))
+                                                ? item[col.name ? col.name : col] : "..."
+                                            }}
+                                        </slot>
+                                    </td>
                                 </slot>
+                            </template>
+                            <td v-if="showActions()"
+                                :class="{'div-y' : divideY || divide}"
+                                class="px-2 py-3 text-center">
                                 <slot :item="item"
-                                      name="action-edit">
-                                    <Link v-if="editRoute"
-                                          :href="route(editRoute, {id:item.id})"
-                                          as="button"
-                                          class="p-1 w-8 h-6">
-                                        <icon class="fill-skin-base  w-5 h-5"
-                                              icon="pencil-alt"></icon>
-                                    </Link>
+                                      name="action">
+                                    <slot :item="item"
+                                          name="action-view">
+                                        <Link v-if="viewRoute"
+                                              :href="route(viewRoute, {id:item.id})"
+                                              as="button"
+                                              class="p-1 w-8 h-6">
+                                            <icon class="fill-skin-base  w-5 h-5"
+                                                  icon="eye"></icon>
+                                        </Link>
+                                    </slot>
+                                    <slot :item="item"
+                                          name="action-edit">
+                                        <Link v-if="editRoute"
+                                              :href="route(editRoute, {id:item.id})"
+                                              as="button"
+                                              class="p-1 w-8 h-6">
+                                            <icon class="fill-skin-base  w-5 h-5"
+                                                  icon="pencil-alt"></icon>
+                                        </Link>
+                                    </slot>
+                                    <slot :item="item"
+                                          name="action-delete">
+                                        <Modal v-if="deleteRoute"
+                                               :href="route(deleteRoute, {id:item.id})"
+                                               :message="deleteMessage ? deleteMessage : __('delete_message')"
+                                               :title="deleteTitle ? deleteTitle : __('delete_title')"
+                                               danger
+                                               method="delete">
+                                            <template #button="{open}">
+                                                <button class="p-1 w-8 h-6"
+                                                        @click="open">
+                                                    <icon class="fill-skin-base  w-5 h-5"
+                                                          icon="trash"></icon>
+                                                </button>
+                                            </template>
+                                        </Modal>
+                                    </slot>
                                 </slot>
-                                <slot :item="item"
-                                      name="action-delete">
-                                    <Modal v-if="deleteRoute"
-                                           :href="route(deleteRoute, {id:item.id})"
-                                           :message="deleteMessage ? deleteMessage : __('delete_message')"
-                                           :title="deleteTitle ? deleteTitle : __('delete_title')"
-                                           danger
-                                           method="delete">
-                                        <template #button="{open}">
-                                            <button class="p-1 w-8 h-6"
-                                                    @click="open">
-                                                <icon class="fill-skin-base  w-5 h-5"
-                                                      icon="trash"></icon>
-                                            </button>
-                                        </template>
-                                    </Modal>
-                                </slot>
-                            </slot>
-                        </td>
-                    </slot>
-                </tr>
-            </slot>
-        </template>
+                            </td>
+                        </slot>
+                    </tr>
+                </slot>
+            </template>
         </tbody>
     </table>
     <Pagination :links="data.meta?.links || data.links"
